8154945f4ed56d2cdec94f19e5d4de46f3b2cb3a
lyric228 <xhikzerox@gmail.com>
2025-05-03 20:01:19 +0500

    db

--- db.py
+++ db.py
@@ -1,15 +1,14 @@
 from json import dump, load, JSONDecodeError
-from os.path import exists as file_exists
+from os.path import exists
 
 
 def save_json(data: dict, filename: str) -> None:
     with open(filename, "w", encoding="utf-8") as file:
         dump(data, file, ensure_ascii=False, indent=4)
 
-
 def load_json(filename: str) -> dict:
     try:
-        if not file_exists(filename):
+        if not exists(filename):
             with open(filename, "w", encoding="utf-8") as file:
                 dump({}, file, ensure_ascii=False, indent=4)
             return {}
